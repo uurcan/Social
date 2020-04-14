80e2f4886c58509d458b963aca9613a1dba3ee8c
ugurcancelebi <42876704+ugurcancelebi@users.noreply.github.com>
2020-04-15 00:12:58 +0300

    Categories & RefreshLayout implementation

--- app/src/main/java/com/example/social/adapter/CategoriesAdapter.java
+++ app/src/main/java/com/example/social/adapter/CategoriesAdapter.java
@@ -17,34 +17,45 @@ import java.util.List;
 public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.ViewHolder> {
     private List<Category> categoryList;
     private Context context;
+    private OnItemClickListener onItemClickListener;
 
     public CategoriesAdapter(List<Category> categoryList, Context context) {
         this.categoryList = categoryList;
         this.context = context;
     }
-
     @NonNull
     @Override
     public CategoriesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.item_categories,parent,false);
         return new ViewHolder(view);
     }
-
     @Override
     public void onBindViewHolder(@NonNull CategoriesAdapter.ViewHolder holder, int position) {
         holder.textCategory.setText(categoryList.get(position).getName());
-    }
 
+    }
     @Override
     public int getItemCount() {
         return categoryList.size();
     }
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener){
+        this.onItemClickListener = onItemClickListener;
+    }
+    public interface OnItemClickListener {
+        void onClick(View view, int position);
+    }
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private TextView textCategory;
+
         ViewHolder(@NonNull View itemView) {
             super(itemView);
             textCategory = itemView.findViewById(R.id.categoryName);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onItemClickListener.onClick(itemView,getAdapterPosition());
         }
     }
 }
